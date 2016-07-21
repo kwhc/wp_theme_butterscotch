79a528705cf020aa262f9bfaeab07dee91a3984b
Kent W <kent.wilhelm@gmail.com>
2016-07-21 12:14:43 -0400

    Clean up indents

--- butterscotch/article-footer.php
+++ butterscotch/article-footer.php
@@ -72,12 +72,12 @@
                 <div class="createsend-button" style="height:22px;display:inline-block;" data-listid="r/7E/EC4/6AE/350309872C70E028"></div>
                 <script type="text/javascript">
                 (function () { var e = document.createElement('script');
-                e.type = 'text/javascript';
-                e.async = true;
-                e.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://btn.createsend1.com/js/sb.min.js?v=3';
-                e.className = 'createsend-script';
-                var s = document.getElementsByTagName('script')[0];
-                s.parentNode.insertBefore(e, s); })();
+                  e.type = 'text/javascript';
+                  e.async = true;
+                  e.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://btn.createsend1.com/js/sb.min.js?v=3';
+                  e.className = 'createsend-script';
+                  var s = document.getElementsByTagName('script')[0];
+                  s.parentNode.insertBefore(e, s); })();
                 </script>
             </li>
 
